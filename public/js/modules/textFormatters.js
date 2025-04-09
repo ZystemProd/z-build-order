32b5e85dcd1fd340f59c8a51a3d0844a76cfe82c
Zystem <113477408+ZystemProd@users.noreply.github.com>
2025-04-09 16:09:20 +0200

    @100% done with underlined word done

--- public/js/modules/textFormatters.js
+++ public/js/modules/textFormatters.js
@@ -77,6 +77,8 @@ function matchActorsWithTrie(actionText, actorTrie) {
   const words = actionText.split(/\s+/);
   const result = [];
   let i = 0;
+  let underlineNextWord = false;
+
 
   while (i < words.length) {
     let currentNode = actorTrie;
@@ -135,6 +137,14 @@ function matchActorsWithTrie(actionText, actorTrie) {
         category === "resource" ? previousWord : null
       );
 
+      let finalFormattedTerm = formattedTerm;
+
+      if (underlineNextWord) {
+        finalFormattedTerm = `<span class="done-underline">${formattedTerm}</span>`;
+        underlineNextWord = false; // reset so only one word is underlined
+      }
+
+
       // Retrieve any punctuation from the last word and append it
       let lastOriginalWord = words[end];
       let cleanLastWord = lastOriginalWord.replace(/[.,;!?]+$/, "");
@@ -145,12 +155,29 @@ function matchActorsWithTrie(actionText, actorTrie) {
         result.pop();
       }
 
-      result.push(formattedTerm + punctuation);
+      result.push(finalFormattedTerm + punctuation);
       i = end + 1;
     } else {
-      result.push(words[i]);
-      i++;
+      const rawWord = words[i];
+      const cleanWord = rawWord.replace(/[.,;!?]+$/, "").toLowerCase();
+      let punctuation = rawWord.slice(cleanWord.length);
+    
+      if (/^@?100%?$/.test(cleanWord)) {
+        result.push(`
+          <svg xmlns="http://www.w3.org/2000/svg" class="inline-icon" height="20px" viewBox="0 -960 960 960" width="20px" fill="#75FB4C">
+            <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+          </svg>${punctuation}
+        `.trim());
+    
+        underlineNextWord = true; // next word should be underlined (if it's matched)
+        i++;
+      } else {
+        result.push(words[i]);
+        i++;
+      }
     }
+     
+    
   }
 
   return result.join(" ");
@@ -164,6 +191,8 @@ function determineUnitClass(unitName) {
   if (units.terran.includes(unit)) return "bold-red";
   return ""; // Default: No class
 }
+
+
 
 // Preprocess abbreviations before matching actors
 function preprocessAbbreviations(actionText) {
