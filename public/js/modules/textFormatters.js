90ca68b7b0c4828ec9c9edfb7e1ff1d1ce7bfe88
Zystem <113477408+ZystemProd@users.noreply.github.com>
2025-04-09 16:34:17 +0200

    add other % for production in formatting

--- public/js/modules/textFormatters.js
+++ public/js/modules/textFormatters.js
@@ -78,6 +78,8 @@ function matchActorsWithTrie(actionText, actorTrie) {
   const result = [];
   let i = 0;
   let underlineNextWord = false;
+  let orangeUnderlineNextWord = false;
+
 
 
   while (i < words.length) {
@@ -141,8 +143,12 @@ function matchActorsWithTrie(actionText, actorTrie) {
 
       if (underlineNextWord) {
         finalFormattedTerm = `<span class="done-underline">${formattedTerm}</span>`;
-        underlineNextWord = false; // reset so only one word is underlined
+        underlineNextWord = false;
+      } else if (orangeUnderlineNextWord) {
+        finalFormattedTerm = `<span class="producing-underline">${formattedTerm}</span>`;
+        orangeUnderlineNextWord = false;
       }
+      
 
 
       // Retrieve any punctuation from the last word and append it
@@ -163,19 +169,33 @@ function matchActorsWithTrie(actionText, actorTrie) {
       let punctuation = rawWord.slice(cleanWord.length);
     
       if (/^@?100%?$/.test(cleanWord)) {
+        // ✅ DONE: Green Underline + Checkmark SVG
         result.push(`
           <svg xmlns="http://www.w3.org/2000/svg" class="inline-icon" height="20px" viewBox="0 -960 960 960" width="20px" fill="#75FB4C">
             <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
           </svg>${punctuation}
         `.trim());
     
-        underlineNextWord = true; // next word should be underlined (if it's matched)
+        underlineNextWord = true;
+        i++;
+      } else if (/^@?\d{1,3}%$/.test(cleanWord)) {
+        // 🟠 PRODUCING: Orange Underline + Producing SVG + Show Number
+        const percentNumber = cleanWord.replace("@", "");
+        result.push(`
+          <svg xmlns="http://www.w3.org/2000/svg" class="inline-icon" height="20px" viewBox="0 -960 960 960" width="20px" fill="#F19E39">
+            <path d="M200-360q-50 0-85-35t-35-85q0-50 35-85t85-35h560q50 0 85 35t35 85q0 50-35 85t-85 35H200Zm360-80h200q17 0 28.5-11.5T800-480q0-17-11.5-28.5T760-520H560v80Z"/>
+          </svg>
+          <span class="producing-percent">${percentNumber}</span>${punctuation}
+        `.trim());
+    
+        orangeUnderlineNextWord = true;
         i++;
       } else {
         result.push(words[i]);
         i++;
       }
     }
+    
      
     
   }
